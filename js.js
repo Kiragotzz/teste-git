2db77be40ffacc23f272bde5c34e80eceab669d6
Luis <luis_gabriell1@hotmail.com>
2020-07-02 09:23:15 -0300

    Ajuste relogio; ajuste função no arquivo .js

--- js.js
+++ js.js
@@ -1,9 +1,16 @@
+
 function relogioHMS() {
-    var d = new Date()
-    var horas = d.getHours()
-    var mins = d.getMinutes()
-    var secs = d.getSeconds()
+    var d = new Date();
+    var horas = d.getHours();
+    var mins = d.getMinutes();
+    var secs = d.getSeconds();
+    var a = document.getElementById("demo");
+    var arr = a.childNodes;
+    for(var x=0;x<arr.length;x++) {
+    	arr[x].innerHTML = horas+":"+mins+":"+secs;
+    }
     //below we have set in the DOM from html <h>
-    document.h.innerHTML = horas+":"+mins+":"+secs
+    
 }
-setInterval(relogioHMS, 1000)
+setInterval(relogioHMS, 1000);
+
